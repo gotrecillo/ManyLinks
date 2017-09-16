3ef0aadb659a3dd70177a06903226acf8217ccc9
Unknown <sergio.panadero.perez@gmail.com>
2017-09-17 00:54:09 +0200

    model autocompletition using explicit file

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,27 +9,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Backpack\Base\app\Notifications\ResetPasswordNotification as ResetPasswordNotification;
 use Laravel\Passport\HasApiTokens;
 
-/**
- * ManyLinks\Models\User
- *
- * @mixin \Eloquent
- * @mixin \Illuminate\Database\Eloquent\Builder
- * @property \Carbon\Carbon|null $created_at
- * @property string $email
- * @property int $id
- * @property string $name
- * @property string $password
- * @property string|null $remember_token
- * @property \Carbon\Carbon|null $updated_at
- * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
- * @method static \Illuminate\Database\Eloquent\Builder|\ManyLinks\Models\User whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\ManyLinks\Models\User whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\ManyLinks\Models\User whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\ManyLinks\Models\User whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\ManyLinks\Models\User wherePassword($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\ManyLinks\Models\User whereRememberToken($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\ManyLinks\Models\User whereUpdatedAt($value)
- */
 class User extends Authenticatable
 {
     use Notifiable;
